ee3aa94abdb57ee6ecd7704f70ce65968340e19d
JulioCPSJr <juliocpsjunior@hotmail.com>
2023-03-18 12:56:51 -0300

    Update index.js

--- Calculadora/index.js
+++ Calculadora/index.js
@@ -4,12 +4,27 @@ const readline = require("readline-sync");
 
 //Function main
 function start(){
-    console.log("Calculator for sum from two numbers");
-    let number = readline.questionInt("Input the first number: ");
+    console.log("Calculator from two numbers");
+    console.log("Select the operation math - 1.Sum 2.Sub 3.Div 4.Mult");
+    let oper = readline.questionInt("\nInput the operation Math ");
+    let number = readline.questionInt("\nInput the first number: ");
     let number2 = readline.questionInt("Input the second number: ");
-
-    let sum = number + number2;
-    console.log("\nThe sum is: ",sum);
+    let result;
+    switch(oper){
+        case 1:
+            result = number + number2;
+            break;
+        case 2:
+            result = number - number2;
+            break;
+        case 3:
+            result = number/number2;
+            break;
+        case 4: 
+            result = number * number2;
+            break;
+    }
+    console.log("\nThe result is: ",result);
     console.log("\nFinish program.");
 }
 
