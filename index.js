e656971c9cfdf9ec97b9ea95b9472ee3fd92bc05
JulioCPSJr <juliocpsjunior@hotmail.com>
2023-03-18 11:56:13 -0300

    Calculadora

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
+//Declara uma constante do tipo readline-sync
+const readline = require("readline-sync");
 
 
 //função principal do nosso sistema
-
 function start(){
+    console.log("Calculator for sum from two numbers");
+    let numero = readline.questionInt("Entre com o primeiro numero: ");
+    let numero2 = readline.questionInt("Entre com o segundo numero: ");
 
+    let sum = numero + numero2;
+    console.log("The sum is: ",sum);
     console.log("Fim de Programa.");
 }
 
