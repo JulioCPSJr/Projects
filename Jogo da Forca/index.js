69716b00f710c32a40d9b4a30572dd02e7370c7b
JulioCPSJr <juliocpsjunior@hotmail.com>
2023-03-20 09:35:03 -0300

    Update index.js

--- Jogo da Forca/index.js
+++ Jogo da Forca/index.js
@@ -12,27 +12,30 @@ function start(){
     let word2 = word;
     let lenword = word.length;
     console.log(`The word have ${lenword} letters`);
-    
+    let count = 0;
+    let failed = 0;
     for (i =0; i < lenword;i++){
     
         let letter = readline.question("Input the letter ");
         let check = word.indexOf(letter);
         let lenletter = word.length;
-        console.log(`${lenletter}`);
-
         if (check >= 0){
             console.log("The letter has in the word");
             word = word.replace(`${letter}`,'');
-            console.log(`${word}`);
+            count = count +1;
         }
         else{
             console.log("The letter hasn't in the word");
+            failed = failed + 1;
         }
-        if (lenletter <= 0 && lenword ==1){
+        if (lenletter <= 0 && lenword == count){
             console.log(`Congratulations !!, the word is ${word2}`);
+            count = 0;
         }
-        else{
+        if (lenletter <= 3 && failed >=1 ){
             console.log(`Failed !!, the word is ${word2}`);
+            failed =0;
+            break;
         }
     };
 
